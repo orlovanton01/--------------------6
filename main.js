b9b8a5154170169e669088be8bf2467626be74a3
orlovanton01 <aov-perm@mail.ru>
2022-12-09 17:46:14 +0500

    second commit

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-let obj={
-    name: 'A',
-    surmane: 'B',
-    patronymic: 'C',
-    data: '1',
-    year: '2001',
-    faculty: 'D'
-}
-
 let mas=[];
-mas.push(obj);
-const btn=document.createElement('button');//создаём кнопку "Начать игру"
+const btn=document.createElement('button');
 document.body.append(btn);
 btn.textContent='Добавить студента';
 btn.className='add';
-let elem;
 btn.addEventListener('click', ()=>{
-    elem=document.getElementsByClassName('name');
-    
-    console.log(document.getElementsByClassName('name'));
-    console.log(elem.input.name.value);
+    let elem1=document.getElementsByClassName('name')[0];
+    let elem2=document.getElementsByClassName('surname')[0];
+    let elem3=document.getElementsByClassName('patronymic')[0];
+    let elem4=document.getElementsByClassName('date')[0];
+    let elem5=document.getElementsByClassName('year')[0];
+    let elem6=document.getElementsByClassName('faculty')[0];
     let obj={
-        name: document.getElementsByClassName('name'),
-        // surmane: document.getElementsByClassName('name'),
-        // patronymic: document.getElementsByClassName('name'),
-        // data: document.getElementsByClassName('name'),
-        // year: document.getElementsByClassName('name'),
-        // faculty: document.getElementsByClassName('name')
+        name: elem1.value,
+        surname: elem2.value,
+        patronymic: elem3.value,
+        date: elem4.value,
+        year: elem5.value,
+        faculty: elem6.value
     }
-
+    mas.push(obj);
+    let student=document.createElement('div');
+    let p=document.createElement('p');
+    document.body.append(p);
+    document.body.append(student);
+    student.textContent=obj.name+' '+obj.surname+' '+obj.patronymic+' '+obj.date+' '+obj.year+' '+obj.faculty;
 });
